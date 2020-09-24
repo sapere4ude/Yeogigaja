08acc8cc371b71f07217f826ee0412708c1abba8
eileenyoo2 <eileenyoo2@gmail.com>
2020-09-24 15:47:01 +0900

    first commit 입니다

---
+++ Yeogigaja/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yeogigaja
+//
+//  Created by sapere4ude on 2020/09/24.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
