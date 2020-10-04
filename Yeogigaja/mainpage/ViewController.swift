6eb34a7a336744f03272c8f19cced4f546884894
SongSeoYoung <bgd216@naver.com>
2020-10-04 18:27:08 +0900

    [modify] main TableView UI

--- Yeogigaja/mainpage/ViewController.swift
+++ Yeogigaja/mainpage/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    //MARK:- properties
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    let cellIdentifier = "mainPageTableViewCell"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+}
+
+extension ViewController:UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mainTableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for:indexPath)
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+        return 170
+    }
+    
+    
 }
 
--- Yeogigaja/mainpage/mainPageTableViewCell.swift
+++ Yeogigaja/mainpage/mainPageTableViewCell.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class mainPageTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var entryImageView: UIImageView!
+    @IBOutlet weak var entryTitleLabel: UILabel!
+    @IBOutlet weak var entryDetailLabel: UILabel!
+    @IBOutlet weak var entryAreaLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
