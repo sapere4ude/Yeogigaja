53521739911f6a19d0f1d89c4e01be1907cfa02f
SongSeoYoung <bgd216@naver.com>
2020-10-04 16:59:26 +0900

    [add] tableView UI 구성

---
+++ Yeogigaja/mainpage/mainPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  mainPageTableViewCell.swift
+//  Yeogigaja
+//
+//  Created by 송서영 on 2020/10/04.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class mainPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
