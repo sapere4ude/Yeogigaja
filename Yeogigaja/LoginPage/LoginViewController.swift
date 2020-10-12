c3a32d80513558e8860bf83bca19023a45e747cc
eileenyoo2 <eileenyoo2@gmail.com>
2020-10-12 22:42:09 +0900

    로그인,회원가입 진행

---
+++ Yeogigaja/LoginPage/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Yeogigaja
+//
+//  Created by sapere4ude on 2020/10/12.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Yeogigaja/RegisterPage/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  Yeogigaja
+//
+//  Created by sapere4ude on 2020/10/12.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
