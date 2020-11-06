d6d1578c337b69b6733313d094da6b1c6304c6af
SongSeoYoung <bgd216@naver.com>
2020-11-06 23:40:34 +0900

    [fix] mapview collectionView 열

--- Yeogigaja/LocationView/locationViewController.swift
+++ Yeogigaja/LocationView/locationViewController.swift
@@ -13,6 +13,7 @@ class locationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tabBarController?.tabBar.isHidden = true
     }
     
     
--- Yeogigaja/MapPage/mapViewController.swift
+++ Yeogigaja/MapPage/mapViewController.swift
@@ -13,8 +13,8 @@ class mapViewController: UIViewController,  UICollectionViewDataSource {
     
     var cellWidth: CGFloat = 0
     var cellHeight: CGFloat = 0
-    var padding: CGFloat = 10
-    var numberOfCol: CGFloat = 2
+    var padding: CGFloat = 20
+    var numberOfCol: CGFloat = 3
     
     
     @IBOutlet weak var localCollectionView: UICollectionView!
@@ -42,23 +42,23 @@ class mapViewController: UIViewController,  UICollectionViewDataSource {
         
         let list = ["은평구", "마포구", "서대문구", "종로구", "중구", "용산구", "성북구", "강북구", "도봉구", "노원구", "중랑구", "광진구", "동대문구", "성동구", "강서구", "양천구", "구로구", "영등포구", "동작구", "금천구", "관악구", "서초구", "강남구", "송파구", "강동구"]
         cell.name.text = list[indexPath.row]
-                cell.backgroundView = UIView()
-                let gradientLayer = CAGradientLayer()
-                gradientLayer.frame.size = cell.frame.size
-                gradientLayer.colors = [#colorLiteral(red: 0.9338900447, green: 0.4315618277, blue: 0.2564975619, alpha: 1),#colorLiteral(red: 0.8518816233, green: 0.1738803983, blue: 0.01849062555, alpha: 1)]
-                gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-                gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-                cell.backgroundView?.layer.addSublayer(gradientLayer)
+        
+        
 
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("selected")
+
         let sb = UIStoryboard(name: "locationView", bundle: nil)
         guard let vc = sb.instantiateViewController(withIdentifier: "location") as? locationViewController else{fatalError()}
         self.navigationController?.pushViewController(vc, animated: true)
-//        self.present(vc, animated: true, completion: nil)
+
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
     }
     
     
