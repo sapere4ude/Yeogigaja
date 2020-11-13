a9db215c1529c4bb28e4b205270afab3f411e8ad
SongSeoYoung <bgd216@naver.com>
2020-11-14 04:32:50 +0900

    [add] custom clicked button

--- Yeogigaja/MapPage/mapViewController.swift
+++ Yeogigaja/MapPage/mapViewController.swift
@@ -7,24 +7,38 @@
 //
 
 import UIKit
+import GEOSwift
+import geos
+import MapKit
+extension UIButton{
 
-class mapViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = event!.touches(for: self)?.first {
+            let location = touch.location(in: self)
+            if alphaFromPoint(point: location) == 0 {
+                self.cancelTracking(with: nil)
+                print("cancelled!")
+            } else{
+                super.touchesBegan(touches, with: event)
+            }
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+    func alphaFromPoint(point: CGPoint) -> CGFloat {
+        var pixel: [UInt8] = [0, 0, 0, 0]
+        let colorSpace = CGColorSpaceCreateDeviceRGB();
+        let alphaInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: &pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: alphaInfo.rawValue)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        context!.translateBy(x: -point.x, y: -point.y)
+        self.layer.render(in: context!)
+
+        let floatAlpha = CGFloat(pixel[3])
+        return floatAlpha
     }
-    */
+}
+class mapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+
 
 }
