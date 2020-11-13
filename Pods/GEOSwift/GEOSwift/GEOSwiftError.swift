dec74c214f66c0fc307a8922cbb49d3a3689e957
SongSeoYoung <bgd216@naver.com>
2020-11-13 10:34:55 +0900

    [add] GEOSwift

---
+++ Pods/GEOSwift/GEOSwift/GEOSwiftError.swift
@@ -0,0 +1,13 @@
+public enum GEOSwiftError: Error, Equatable {
+    case invalidJSON
+    case invalidGeoJSONType
+    case invalidCoordinates
+    case mismatchedGeoJSONType
+    case tooFewPoints
+    case ringNotClosed
+    case tooFewRings
+    case invalidFeatureId
+    case lengthIsZero
+    case unexpectedEnvelopeResult(Geometry)
+    case negativeBufferWidth
+}
