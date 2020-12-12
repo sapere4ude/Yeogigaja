8627f6783cec14296add672ccbdfc5342aaa1318
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-12 13:56:04 +0900

    [add]공지사항 뷰

--- Yeogigaja/Mypage/Notification/NotificationViewController.swift
+++ Yeogigaja/Mypage/Notification/NotificationViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 class NotificationViewController: UIViewController {
 
+    @IBOutlet weak var notifiTableView: UITableView!
+    
+    // 서버에서 원격으로 보내는 데이터 갯수에 따라 달라지리 변수
+    var notifiNumber = 1
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK:- tableView delegate
+extension NotificationViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifiNumber
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: notifiTableViewCell = notifiTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! notifiTableViewCell
+        //서버 IndexPath.row 에 따라 다른 내용으로 cell label 을 바꿔야함
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "notification") as? NotifiViewController else{return}
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
 }
--- Yeogigaja/Mypage/Notification/notifiTableViewCell.swift
+++ Yeogigaja/Mypage/Notification/notifiTableViewCell.swift
@@ -10,15 +10,8 @@ import UIKit
 
 class notifiTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
 
 }
