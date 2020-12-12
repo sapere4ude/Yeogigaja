5e415bd9679ac93d4722b917ace668563d45cfe7
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-12 13:55:35 +0900

    공지사항 뷰

---
+++ Yeogigaja/Mypage/FavLocation/locationData.swift
@@ -0,0 +1,9 @@
+//
+//  locationData.swift
+//  Yeogigaja
+//
+//  Created by 송서영 on 2020/12/12.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import Foundation
---
+++ Yeogigaja/Mypage/Notification/notifiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  notifiTableViewCell.swift
+//  Yeogigaja
+//
+//  Created by 송서영 on 2020/12/12.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class notifiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
