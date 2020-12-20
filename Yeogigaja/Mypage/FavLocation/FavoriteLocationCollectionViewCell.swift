224600cc0987d9503476bd8880c66b7ac5e88435
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-20 14:52:30 +0900

    [add] favorite location collectionView

---
+++ Yeogigaja/Mypage/FavLocation/FavoriteLocationCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteLocationCollectionViewCell.swift
+//  Yeogigaja
+//
+//  Created by 송서영 on 2020/12/20.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteLocationCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+}
+
+
--- Yeogigaja/Mypage/FavLocation/FavoriteLocationViewController.swift
+++ Yeogigaja/Mypage/FavLocation/FavoriteLocationViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class FavoriteLocationViewController: UIViewController {
 
+    let SeoulLocation = ["종로구", "중구", "용산구", "성동구", "광진구", "동대문구", "종랑구", "성북구", "강북구", "도봉구", "노원구", "은평구", "서대문구", "마포구", "양천구", "강서구", "구로구", "금천구", "영등포구", "동작구", "관악구", "서초구", "강남구", "송파구"]
+    @IBOutlet weak var locationCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FavoriteLocationViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+        //서울 구 갯수
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = locationCollectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteLocationCollectionViewCell", for: indexPath) as! FavoriteLocationCollectionViewCell
+        cell.locationLabel.text = SeoulLocation[indexPath.row]
+        return cell
+    }
+    
+    
 }
