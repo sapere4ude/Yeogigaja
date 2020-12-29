047d5d7c3d0d667832dd6ffee759d10c3a230be6
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-29 18:13:06 +0900

    [add] 공지사항 디테일 페이지

---
+++ Yeogigaja/Mypage/Notification/NotifiDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotifiDetailViewController.swift
+//  Yeogigaja
+//
+//  Created by 송서영 on 2020/12/29.
+//  Copyright © 2020 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class NotifiDetailViewController: UIViewController {
+
+    @IBOutlet weak var NotifiDateLabel: UILabel!
+    @IBOutlet weak var NotifiTextView: UITextView!
+    @IBOutlet weak var NotifiTitleLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    //Data fetch 필요
+
+}
