8f901097966b9e44595e2d5c1bce44ec3a087ff5
seoyoung <seoyoung_00@hufs.ac.kr>
2020-12-31 16:05:48 +0900

    [feat] 설정 TableView

--- Yeogigaja/Mypage/Settings/SettingViewController.swift
+++ Yeogigaja/Mypage/Settings/SettingViewController.swift
@@ -11,24 +11,45 @@ import UIKit
 class SettingViewController: UIViewController {
 
     @IBOutlet private weak var SettingTableView: UITableView!
-    let settingTitle: [SettingModel] = [
+    private let settingTitle: [SettingModel] = [
         SettingModel("회원 정보 수정"),
         SettingModel("로그아웃"),
         SettingModel("회원 탈퇴"),
         SettingModel("이용 약관"),
-        SettingModel("개인정보 처리 방침"),
+        SettingModel("개인정보 처리 방침")
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.SettingTableView.dataSource = self
+        self.SettingTableView.delegate = self
+        print("didload")
     }
+
 }
+
+
+//MARK:- extension TableView
 extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        print(settingTitle.count)
+        return settingTitle.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell: SettingTableViewCell = SettingTableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as? SettingTableViewCell else { return UITableViewCell() }
+        cell.SettingLabel.text = settingTitle[indexPath.row].SettingLabel
+        print(settingTitle[indexPath.row].SettingLabel)
+        print(cell.SettingLabel.text)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let vc = self.storyboard?.instantiateViewController(identifier: "SettingDetailViewController") else { return }
+        if indexPath.row == 0 {
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        else{
+            print("else")
+        }
     }
     
     
