e25c58e8cd9fc9516fd27fde5f76f060ba345f6f
seoyoung <seoyoung_00@hufs.ac.kr>
2021-01-09 21:01:15 +0900

    [add] user 정보 수정페이지

--- Yeogigaja/Mypage/Settings/SettingDetailViewController.swift
+++ Yeogigaja/Mypage/Settings/SettingDetailViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class SettingDetailViewController: UIViewController {
 
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userId: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    @IBOutlet weak var userPasswordCheck: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
