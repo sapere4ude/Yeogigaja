c6ad23d4938dfbb4819824706dc231cb2cad8da3
Jintae Kim <dd3557@naver.com>
2021-01-11 14:06:28 +0900

    Master와 병합하기 위한 commit

--- Yeogigaja/DetailPage/DetailViewController.swift
+++ Yeogigaja/DetailPage/DetailViewController.swift
@@ -8,100 +8,110 @@
 
 import Parchment
 import UIKit
-import FirebaseDatabase
-import FirebaseAuth
 
 class DetailViewController: UITableViewController {
+    var data: WritePage?
     
-//    var fetchInfos: [fetchInfo] = []
-    var titleLabel: String? = ""
-    var descriptionLabel: String? = ""
-    var withFriendsLabel: String? = ""
-    var locationLabel: String? = ""
-    
-    @IBOutlet weak var headerImageView: UIImageView!
-    
+    //    var fetchInfos: [fetchInfo] = []
+//        var titleLabel: String? = ""
+//        var descriptionLabel: String? = ""
+//        var withFriendsLabel: String? = ""
+//        var locationLabel: String? = ""
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.contentInsetAdjustmentBehavior = .never
         tableView.separatorStyle = .none
-        print("얍")
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        navigationController?.setThemeAsDarkTranslucent()
-//
-//        let userEmail = Auth.auth().currentUser?.email
-//        var safeEmail = userEmail!.replacingOccurrences(of: ".", with: "-") // 문자열에서 원하는 문자 다른것으로 대체
-//        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
-//
-//        let postDatabaseRef = Database.database().reference().child("\(safeEmail)").child("Contents")
-//        postDatabaseRef.observeSingleEvent(of: .value) { (snapshot) in
-//            guard let postInfo = snapshot.value as? [String: Any] else { return }
-//            let data = try! JSONSerialization.data(withJSONObject: Array(postInfo.values), options: [])
-//            print("data--->\(data)")
-//            let decoder = JSONDecoder()
-//            let fetchInfos = try! decoder.decode([fetchInfo].self, from: data)  // data -> [fetchInfo].self 형태로 디코딩
-//            self.fetchInfos = fetchInfos
-////            self.tableView.reloadData()
-//            print("snapshot--->\(data), \(fetchInfos)")
-//        }
-//    }
-    
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        super.viewWillAppear(animated)
+    //        navigationController?.setThemeAsDarkTranslucent()
+    //
+    //        let userEmail = Auth.auth().currentUser?.email
+    //        var safeEmail = userEmail!.replacingOccurrences(of: ".", with: "-") // 문자열에서 원하는 문자 다른것으로 대체
+    //        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+    //
+    //        let postDatabaseRef = Database.database().reference().child("\(safeEmail)").child("Contents")
+    //        postDatabaseRef.observeSingleEvent(of: .value) { (snapshot) in
+    //            guard let postInfo = snapshot.value as? [String: Any] else { return }
+    //            let data = try! JSONSerialization.data(withJSONObject: Array(postInfo.values), options: [])
+    //            print("data--->\(data)")
+    //            let decoder = JSONDecoder()
+    //            let fetchInfos = try! decoder.decode([fetchInfo].self, from: data)  // data -> [fetchInfo].self 형태로 디코딩
+    //            self.fetchInfos = fetchInfos
+    ////            self.tableView.reloadData()
+    //            print("snapshot--->\(data), \(fetchInfos)")
+    //        }
+    //    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setThemeAsDarkTranslucent()
+    }
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setThemeAsDefault()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        print(#function)
-//        print("fetchInfos.count--->\(fetchInfos.count)")
-        return 2
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        switch indexPath.row {
-//        case 0:
-//            print(#function, "fetchInfos--->\(fetchInfos)")
-//            let cellIdentifier = String(describing: DetailTableViewCell.self)
-//            let cell: DetailTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! DetailTableViewCell
-//            cell.titleLabel.text = "메모"
-//            cell.infoLabel.text = "2020/11/02 (월)"
-//            cell.descriptionLabel.text = "영화 의뢰인 속 배우 하정우도 맛있게 흡입한 현지의 맛 가득 담은 베트남 음식점!! 00 이랑 다음에 가야겠다:-)"
-//            cell.descriptionLabel.text = fetchInfos[indexPath.row].description
-            
-//            해결해야하는 부분!
-            
-//            cell.selectionStyle = .none
-//
-//            cell.separatorView.isHidden = false
-//            return cell
-//        case 1:
-            let cellIdentifier = String(describing: DetailTableViewCell.self)
-            let cell: DetailTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! DetailTableViewCell
-        cell.titleLabel.text = titleLabel
-        cell.infoLabel.text = withFriendsLabel
-        cell.descriptionLabel.text = descriptionLabel
-//            cell.titleLabel.text = "같이 가고 싶은 사람"
-//            cell.infoLabel.text = "총 5명"
-//            cell.descriptionLabel.text = "ㅇㅇㅇ / ㅇㅇㅇ / ㅇㅇㅇ / ㅇㅇㅇ / ㅇㅇㅇ"
-        
-//            cell.selectionStyle = .none
-//
-//            cell.separatorView.isHidden = true
+        switch indexPath.row {
+        case 0:
+            let cellIdentifier = String(describing: DetailTableViewPostCell.self)
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! DetailTableViewPostCell
+
+            cell.titleLabel.text = data?.name
+            cell.descriptionLabel.text = data?.description
+            cell.usernameLabel.text = "더미 데이터 (코드에 상수 대입)"
+
+            // NSMutableAttributedString에서 사용할 로케이션 아이콘을 NSTextAttachment를 이용해 설정
+            let locationIconImage = UIImage(named: "map")?.withTintColor(cell.locationButton.tintColor, renderingMode: .alwaysTemplate)
+            let locationIconTextAttachment = NSTextAttachment()
+            locationIconTextAttachment.image = locationIconImage
+
+            let locationIconWidth: Double = 20.0
+
+            let locationIconHeight = locationIconWidth
+
+            // 로케이션 아이콘을 텍스트 라인의 가운데로 정렬 및 아이콘 크기 지정
+            locationIconTextAttachment.bounds = CGRect(x: 0.0, y: (Double(cell.locationButton.titleLabel?.font.capHeight ?? 0.0) - locationIconHeight) / 2.0, width: locationIconWidth, height: locationIconHeight)
+
+            // NSMutableAttributedString을 이용하여 텍스트 맨 앞에 로케이션 아이콘을 문자처럼 넣음
+            let locationText = NSMutableAttributedString()
+            locationText.append(NSAttributedString(attachment: locationIconTextAttachment))
+            // \u{200B}는 길이가 없는 문자열이다. 아래의 addAttribute를 이용하기 위해서 넣어주었다.
+            locationText.append(NSAttributedString(string: "\u{200B}\(data?.location ?? "장소 데이터 없음")"))
+
+            // 아이콘과 location 문자열 사이에 4만큼 거리 띄어줌
+            locationText.addAttribute(.kern, value: 4, range: NSMakeRange(1, 1))
+            locationText.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(2, data?.location.count ?? 0))
+            cell.locationButton.setAttributedTitle(locationText, for: .normal)
+
+            cell.locationButton.addTarget(self, action: #selector(locationButtonPressed), for: .touchUpInside)
+
+            let image = UIImage(named: "user")
+            cell.userProfileImageView.image = image
+            cell.selectionStyle = .none
 
             return cell
-//        default:
-//            fatalError("Detail View Controller의 cellForRowAt 메소드에서 엉뚱한 인덱스에 접근하였습니다")
-//        }
+        default:
+            fatalError("Detail View Controller의 cellForRowAt 메소드에서 엉뚱한 인덱스에 접근하였습니다")
+        }
     }
-    
+
+    @objc func locationButtonPressed() {
+        print("임시로 버튼 기능 추가")
+    }
 }
 
 extension UINavigationController {
--- Yeogigaja/TableViewPage/TableViewController.swift
+++ Yeogigaja/TableViewPage/TableViewController.swift
@@ -1,146 +1,148 @@
+////
+////  entryTableViewController.swift
+////  Yeogigaja
+////
+////  Created by 송서영 on 2020/10/05.
+////  Copyright © 2020 sapere4ude. All rights reserved.
+////
 //
-//  entryTableViewController.swift
-//  Yeogigaja
+//// MainTableViewController로 대체되는 파일이지만, 서버 관련 코드를 보기 위해 남겨놓은 파일
 //
-//  Created by 송서영 on 2020/10/05.
-//  Copyright © 2020 sapere4ude. All rights reserved.
+//import UIKit
+//import Parchment
+//import FirebaseAuth
+//import FirebaseDatabase
+//import FirebaseStorage
+////import FirebaseUI
 //
-
-import UIKit
-import Parchment
-import FirebaseAuth
-import FirebaseDatabase
-import FirebaseStorage
-//import FirebaseUI
-
-class TableViewController: UIViewController {
-    
-    //MARK:paging
-    let queryOnceCnt:Int = 10
-    var currentPage:Int = 0
-    let callNextPageBeforeOffset:CGFloat = 150
-    var isQuery:Bool = false
-    var reachEnd:Bool = false
-    var searchText:String = ""
-    
-    // MARK: - 서버정보를 받기위한 배열, fetchInfo는 구조체로 구현
-    var fetchInfos: [fetchInfo] = []
-    var postInfo: [String: Any] = [:]
-    
-    @IBOutlet weak var entryTableView: UITableView!
-    
-    let cellIdentifier = "mainPageTableViewCell"
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        entryTableView.separatorStyle = .none
-        entryTableView.delegate = self
-        entryTableView.dataSource = self
-        navigationItem.backButtonTitle = ""
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        let userEmail = Auth.auth().currentUser?.email
-        var safeEmail = userEmail!.replacingOccurrences(of: ".", with: "-") // 문자열에서 원하는 문자 다른것으로 대체
-        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
-        
-        
-        DispatchQueue.main.async {
-            let postDatabaseRef = Database.database().reference().child("\(safeEmail)").child("Contents")
-            postDatabaseRef.observeSingleEvent(of: .value) { (snapshot) in
-                guard let postInfo = snapshot.value as? [[String: Any]] else { return }
-                print("\(postInfo)")
-                let data = try! JSONSerialization.data(withJSONObject: postInfo, options: [])
-                let decoder = JSONDecoder()
-                let fetchInfos = try! decoder.decode([fetchInfo].self, from: data)  // data -> [fetchInfo].self 형태로 디코딩
-                self.fetchInfos = fetchInfos
-                self.entryTableView.reloadData()
-                print("snapshot--->\(data), \(fetchInfos)")
-            }
-        }
-    }
-}
-
-
-//MARK:- tableView extension
-extension TableViewController:UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchInfos.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = entryTableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as? tableViewCell else {
-            return UITableViewCell()
-        }
-        cell.entryTitleLabel?.text = fetchInfos[indexPath.row].name
-        cell.entryDetailLabel?.text = fetchInfos[indexPath.row].description
-        cell.entryAreaLabel?.text = fetchInfos[indexPath.row].location
-        
-        // Create reference to the file whose metadata we want to retrieve
-//        let forestRef = Storage.storage().reference(withPath: fetchInfos[indexPath.row].image ?? "error")
-        
-//        let qq = Storage.storage().reference().child("photos/-MQQkfUbDYCmX-yDHejI.jpg")
-//        print("qq->\(qq)")
-        
-        let storageRef = Storage.storage().reference().child(fetchInfos[indexPath.row].image ?? "error");
-        storageRef.downloadURL { (URL, error) -> Void in
-          if (error != nil) {
-            // Handle any errors
-          } else {
-            // Get the download URL for 'images/stars.jpg'
-            print(URL!)
-            do {
-            let data = try Data(contentsOf: URL!)
-            cell.entryImageView.image = UIImage(data: data)
-            }
-            catch {
-                print("q")
-            }
-          }
-        }
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("did selected")
-        let sb = UIStoryboard(name: "DetailViewStoryboard", bundle: nil)
-        guard let vc = sb.instantiateViewController(withIdentifier: "detailView") as? DetailViewController else{ fatalError() }
-        vc.descriptionLabel = fetchInfos[indexPath.row].description
-        vc.titleLabel = fetchInfos[indexPath.row].location
-        vc.withFriendsLabel = fetchInfos[indexPath.row].withFriends
-        
-        let storageRef = Storage.storage().reference().child(fetchInfos[indexPath.row].image ?? "error");
-        storageRef.downloadURL { (URL, error) -> Void in
-          if (error != nil) {
-            // Handle any errors
-          } else {
-            // Get the download URL for 'images/stars.jpg'
-            print(URL!)
-            do {
-            let data = try Data(contentsOf: URL!)
-            vc.headerImageView.image = UIImage(data: data)
-            }
-            catch {
-                print("q")
-            }
-          }
-        }
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cell.backgroundColor = UIColor.clear
-    }
-    
-}
-
-struct fetchInfo: Codable {
-    let description: String
-    let name: String
-    let location: String
-    let withFriends: String
-    let image: String?
-}
-
-
+//class TableViewController: UIViewController {
+//    
+//    //MARK:paging
+//    let queryOnceCnt:Int = 10
+//    var currentPage:Int = 0
+//    let callNextPageBeforeOffset:CGFloat = 150
+//    var isQuery:Bool = false
+//    var reachEnd:Bool = false
+//    var searchText:String = ""
+//    
+//    // MARK: - 서버정보를 받기위한 배열, fetchInfo는 구조체로 구현
+//    var fetchInfos: [fetchInfo] = []
+//    var postInfo: [String: Any] = [:]
+//    
+//    @IBOutlet weak var entryTableView: UITableView!
+//    
+//    let cellIdentifier = "mainPageTableViewCell"
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        entryTableView.separatorStyle = .none
+//        entryTableView.delegate = self
+//        entryTableView.dataSource = self
+//        navigationItem.backButtonTitle = ""
+//    }
+//    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        let userEmail = Auth.auth().currentUser?.email
+//        var safeEmail = userEmail!.replacingOccurrences(of: ".", with: "-") // 문자열에서 원하는 문자 다른것으로 대체
+//        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+//        
+//        
+//        DispatchQueue.main.async {
+//            let postDatabaseRef = Database.database().reference().child("\(safeEmail)").child("Contents")
+//            postDatabaseRef.observeSingleEvent(of: .value) { (snapshot) in
+//                guard let postInfo = snapshot.value as? [[String: Any]] else { return }
+//                print("\(postInfo)")
+//                let data = try! JSONSerialization.data(withJSONObject: postInfo, options: [])
+//                let decoder = JSONDecoder()
+//                let fetchInfos = try! decoder.decode([fetchInfo].self, from: data)  // data -> [fetchInfo].self 형태로 디코딩
+//                self.fetchInfos = fetchInfos
+//                self.entryTableView.reloadData()
+//                print("snapshot--->\(data), \(fetchInfos)")
+//            }
+//        }
+//    }
+//}
+//
+//
+////MARK:- tableView extension
+//extension TableViewController:UITableViewDataSource, UITableViewDelegate {
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return fetchInfos.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = entryTableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as? tableViewCell else {
+//            return UITableViewCell()
+//        }
+//        cell.entryTitleLabel?.text = fetchInfos[indexPath.row].name
+//        cell.entryDetailLabel?.text = fetchInfos[indexPath.row].description
+//        cell.entryAreaLabel?.text = fetchInfos[indexPath.row].location
+//        
+//        // Create reference to the file whose metadata we want to retrieve
+////        let forestRef = Storage.storage().reference(withPath: fetchInfos[indexPath.row].image ?? "error")
+//        
+////        let qq = Storage.storage().reference().child("photos/-MQQkfUbDYCmX-yDHejI.jpg")
+////        print("qq->\(qq)")
+//        
+//        let storageRef = Storage.storage().reference().child(fetchInfos[indexPath.row].image ?? "error");
+//        storageRef.downloadURL { (URL, error) -> Void in
+//          if (error != nil) {
+//            // Handle any errors
+//          } else {
+//            // Get the download URL for 'images/stars.jpg'
+//            print(URL!)
+//            do {
+//            let data = try Data(contentsOf: URL!)
+//            cell.entryImageView.image = UIImage(data: data)
+//            }
+//            catch {
+//                print("q")
+//            }
+//          }
+//        }
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("did selected")
+//        let sb = UIStoryboard(name: "DetailViewStoryboard", bundle: nil)
+//        guard let vc = sb.instantiateViewController(withIdentifier: "detailView") as? DetailViewController else{ fatalError() }
+//        vc.descriptionLabel = fetchInfos[indexPath.row].description
+//        vc.titleLabel = fetchInfos[indexPath.row].location
+//        vc.withFriendsLabel = fetchInfos[indexPath.row].withFriends
+//        
+//        let storageRef = Storage.storage().reference().child(fetchInfos[indexPath.row].image ?? "error");
+//        storageRef.downloadURL { (URL, error) -> Void in
+//          if (error != nil) {
+//            // Handle any errors
+//          } else {
+//            // Get the download URL for 'images/stars.jpg'
+//            print(URL!)
+//            do {
+//            let data = try Data(contentsOf: URL!)
+//            vc.headerImageView.image = UIImage(data: data)
+//            }
+//            catch {
+//                print("q")
+//            }
+//          }
+//        }
+//        self.navigationController?.pushViewController(vc, animated: true)
+//    }
+//    
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        cell.backgroundColor = UIColor.clear
+//    }
+//    
+//}
+//
+//struct fetchInfo: Codable {
+//    let description: String
+//    let name: String
+//    let location: String
+//    let withFriends: String
+//    let image: String?
+//}
+//
+//
