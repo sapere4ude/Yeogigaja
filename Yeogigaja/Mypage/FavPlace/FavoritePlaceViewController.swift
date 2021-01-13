09860c5c50f938a9698c22e0b1f947224446fd6b
seoyoung <seoyoung_00@hufs.ac.kr>
2021-01-13 18:08:24 +0900

    [add] 찜한장소 UI

--- Yeogigaja/Mypage/FavPlace/FavoritePlaceViewController.swift
+++ Yeogigaja/Mypage/FavPlace/FavoritePlaceViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class FavoritePlaceViewController: UIViewController {
 
+    @IBOutlet weak var favoriteTableView: UITableViewCell!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+}
+extension FavoritePlaceViewController: UITableViewDelegate, UITableViewDataSource {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Yeogigaja/Mypage/FavPlace/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Yeogigaja
+//
+//  Created by 송서영 on 2021/01/13.
+//  Copyright © 2021 sapere4ude. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
